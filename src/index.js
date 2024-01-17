7dfbf6ef79a20eeee96d7a2d77d602766ca29fdb
Chase Anderson <chaseanderson.stu@gmail.com>
2024-01-17 11:02:02 -0500

    complete lab

--- src/index.js
+++ src/index.js
@@ -1,3 +1,37 @@
 document.addEventListener("DOMContentLoaded", () => {
-  // your code here
+  const taskInput = document.getElementById("new-task-description");
+
+  taskInput.addEventListener("input", (event) => {
+    const userInput = event.target.value;
+    console.log(userInput);
+  });
+});
+
+document.addEventListener("DOMContentLoaded", () => {
+  const taskForm = document.getElementById("create-task-form");
+
+  taskForm.addEventListener("submit", (event) => {
+    event.preventDefault();
+    const taskInput = document.getElementById("new-task-description");
+    const userInput = taskInput.value;
+    console.log(userInput);
+  });
+});
+
+document.addEventListener("DOMContentLoaded", () => {
+  const taskForm = document.getElementById("create-task-form");
+  const taskList = document.getElementById("tasks");
+
+  taskForm.addEventListener("submit", (event) => {
+    event.preventDefault();
+
+    const taskInput = document.getElementById("new-task-description");
+    const userInput = taskInput.value;
+
+    const newTask = document.createElement("li");
+    newTask.textContent = userInput;
+
+    taskList.appendChild(newTask);
+    taskInput.value = "";
+  });
 });
